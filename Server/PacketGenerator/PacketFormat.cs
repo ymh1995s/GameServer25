37f1d89058bb80290d8c8bd3c9dec09f2bb9781e
ymh1995s <2014038117ab@gmail.com>
2025-01-01 03:33:41 +0900

    패킷 관련 주석 추가

--- Server/PacketGenerator/PacketFormat.cs
+++ Server/PacketGenerator/PacketFormat.cs
@@ -26,15 +26,20 @@ class PacketManager
 		Register();
 	}}
 
+	// ushort : 패킷 아이디, Action<> : 지금 수신한 이 패킷은 어떤 패킷인가?
 	Dictionary<ushort, Action<PacketSession, ArraySegment<byte>, ushort>> _onRecv = new Dictionary<ushort, Action<PacketSession, ArraySegment<byte>, ushort>>();
+	// ushort : 패킷 아이디, Action<> : _onRecv의 패킷 처리 로직은 무엇인가?
 	Dictionary<ushort, Action<PacketSession, IMessage>> _handler = new Dictionary<ushort, Action<PacketSession, IMessage>>();
 		
 	public Action<PacketSession, IMessage, ushort> CustomHandler {{ get; set; }}
 
+	// Register()는 멀티스레드가 개입 되기 전 호출되어야함
 	public void Register()
 	{{{0}
 	}}
 
+	// 지금 수신한 이 패킷을 딕셔너리에서 찾고
+	// 해당 패킷을 Invoke(여기서는 MakePacket())함
 	public void OnRecvPacket(PacketSession session, ArraySegment<byte> buffer)
 	{{
 		ushort count = 0;
@@ -49,6 +54,9 @@ class PacketManager
 			action.Invoke(session, buffer, id);
 	}}
 
+	// OnRecvPacket()로부터 Invoke로 호출 되어
+	// 이 패킷의 처리 로직을 Invoke(여기서는 PacketHandler.cs의 각 처리 로직)
+	// 유니티는 메인스레드에서 패킷을 처리해야 하기 때문에 분기가 나뉘어진 모습
 	void MakePacket<T>(PacketSession session, ArraySegment<byte> buffer, ushort id) where T : IMessage, new()
 	{{
 		T pkt = new T();
@@ -68,6 +76,8 @@ class PacketManager
 		}}
 	}}
 
+	// 유니티에서 큐에 담아둔 해당 패킷의 처리 로직이 무엇인지
+	// PacketHandler.cs로 유도해주는 함수 
 	public Action<PacketSession, IMessage> GetPacketHandler(ushort id)
 	{{
 		Action<PacketSession, IMessage> action = null;
