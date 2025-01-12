e45ccbe5253cd8c8451909ec4fb732920b15f2f4
ymh1995s <2014038117ab@gmail.com>
2025-01-13 02:08:10 +0900

    추측항법 기초 적용 중 - 키보드를 떼는 순간에 바로 정지가 되도록

--- Client/Assets/Scripts/Controller/MyPlayerController.cs
+++ Client/Assets/Scripts/Controller/MyPlayerController.cs
@@ -6,11 +6,6 @@ public class MyPlayerController : PlayerController
 {
     float moveSpeed = 5f;
 
-    // 카메라
-    private Camera playerCamera;  // 카메라
-    private float rotationSpeed = 1f;  // 회전 속도
-    private float pitch = 0f; // X축 회전 (상하)
-    private float yaw = 0f;   // Y축 회전 (좌우)
     private void Start()
     {
         playerCamera = Camera.main;  // 메인 카메라를 가져옵니다.
@@ -45,20 +40,31 @@ public class MyPlayerController : PlayerController
         {
             destinationPos += Vector3.right;
         }
+
         if (destinationPos != Vector3.zero)
         {
             destinationPos = (destinationPos.normalized * moveSpeed); // 정규화
             destinationPos += transform.position;
-
-            PositionInfo destInfo = new PositionInfo();
-            Vector3ToPosInfo(destinationPos, ref destInfo); // 프로퍼티 참조 반환하는게 별로라 그냥 한번 더 바꿔줌
-
-            C_Move movePacket = new C_Move();
-            movePacket.PosInfo = destInfo;
-            MasterManager.Network.Send(movePacket);
+        }
+        else
+        {
+            destinationPos = transform.position;
         }
 
+        // TODO : 어떻게 해야 매프레임 패킷을 안 보낼 수 있을까?
+        PositionInfo destInfo = new PositionInfo();
+        Vector3ToPosInfo(destinationPos, ref destInfo); // 프로퍼티 참조 반환하는게 별로라 그냥 한번 더 바꿔줌
+
+        C_Move movePacket = new C_Move();
+        movePacket.PosInfo = destInfo;
+        MasterManager.Network.Send(movePacket);
     }
+
+    #region 카메라
+    private Camera playerCamera;  // 카메라
+    private float rotationSpeed = 1f;  // 회전 속도
+    private float pitch = 0f; // X축 회전 (상하)
+    private float yaw = 0f;   // Y축 회전 (좌우)
 
     private void RotatePlayerWithMouse()
     {
@@ -79,4 +85,5 @@ public class MyPlayerController : PlayerController
         playerCamera.transform.localRotation = Quaternion.Euler(pitch, 0f, 0f);  // 카메라의 상하 회전
         transform.rotation = Quaternion.Euler(0f, yaw, 0f);  // 플레이어의 좌우 회전
     }
+    #endregion camera
 }
